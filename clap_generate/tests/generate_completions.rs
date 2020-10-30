8516f660315316a80f81170282b563a0efe979d5
hk <alexhaka10@protonmail.com>
2020-10-30 20:34:51 +0100

    test(clap_generate): adding test to check for zsh completion generation bug

---
+++ clap_generate/tests/generate_completions.rs
@@ -0,0 +1,35 @@
+use clap::{App, Arg};
+use clap_generate::{generate, generators::*};
+use std::io;
+
+#[test]
+fn generate_completions() {
+    let mut app = App::new("test_app")
+        .version("1.0")
+        .author("Kevin K. <kbknapp@gmail.com>")
+        .about("Does awesome things")
+        .arg(
+            Arg::new("config")
+                .short('c')
+                .conflicts_with("v")
+                .global(true),
+        )
+        .arg(
+            Arg::new("v")
+                .short('v')
+                .about("Sets the level of verbosity"),
+        )
+        .subcommand(
+            App::new("test").about("Subcommand").arg(
+                Arg::new("debug")
+                    .short('d')
+                    .about("print debug information verbosely"),
+            ),
+        );
+
+    generate::<Bash, _>(&mut app, "test_app", &mut io::sink());
+    generate::<Fish, _>(&mut app, "test_app", &mut io::sink());
+    generate::<PowerShell, _>(&mut app, "test_app", &mut io::sink());
+    generate::<Elvish, _>(&mut app, "test_app", &mut io::sink());
+    generate::<Zsh, _>(&mut app, "test_app", &mut io::sink());
+}
